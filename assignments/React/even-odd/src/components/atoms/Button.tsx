559286e94be524f8fb0cd7e48a612ac4bc36a694
uday <udaysai.bura@zemosolabs.com>
2026-01-15 18:40:22 +0530

    made changes for even odd

--- assignments/React/even-odd/src/components/atoms/Button.tsx
+++
@@ -1,7 +0,0 @@
-type ButtonProps = {
-  label: string;
-  onClick: () => void;
-};
-export const Button = ({ label, onClick }: ButtonProps) => {
-  return <button onClick={onClick}>{label}</button>;
-};
--- assignments/React/even-odd/src/components/atoms/Text.tsx
+++
@@ -1,8 +0,0 @@
-type TextProps={
-    text: string;
-};
-export const Text=({text}:TextProps)=>{
-    return(
-            <p>{text}</p>
-    )
-}
